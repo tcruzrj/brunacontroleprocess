a35f40899d5a40ad7d677ed2003c8dda94e6f1f7
tcruzrj <tcruzrj@gmail.com>
2025-01-29 09:48:19 -0300

    20250129

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import EditProcess from "./pages/EditProcess";
-import NotFound from "./pages/NotFound";
+import Index from "./pages/Index.tsx";
+import EditProcess from "./pages/EditProcess.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
 
